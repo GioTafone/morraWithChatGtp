5969d87b70e1d268d569349b8ea47a5d4d68ecc3
Giovanni Tafone <giola@Giovannis-MacBook-Air.local>
2023-03-14 21:40:24 +0200

    initail commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+enum Choice {
+  ROCK = 'ROCK',
+  PAPER = 'PAPER',
+  SCISSORS = 'SCISSORS',
+}
+
+const choices: Choice[] = [Choice.ROCK, Choice.PAPER, Choice.SCISSORS];
+
+const getResult = (playerChoice: Choice, computerChoice: Choice) => {
+  if (playerChoice === computerChoice) {
+    return 'Tie';
+  } else if (
+    (playerChoice === Choice.ROCK && computerChoice === Choice.SCISSORS) ||
+    (playerChoice === Choice.PAPER && computerChoice === Choice.ROCK) ||
+    (playerChoice === Choice.SCISSORS && computerChoice === Choice.PAPER)
+  ) {
+    return 'You Win';
+  } else {
+    return 'Computer Wins';
+  }
+};
+
+const App = () => {
+  const [playerChoice, setPlayerChoice] = useState<Choice | null>(null);
+  const [computerChoice, setComputerChoice] = useState<Choice | null>(null);
+  const [result, setResult] = useState<string | null>(null);
+
+  const handlePlayerChoice = (choice: Choice) => {
+    const computerRandomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setPlayerChoice(choice);
+    setComputerChoice(computerRandomChoice);
+    setResult(getResult(choice, computerRandomChoice));
+  };
+
+  const resetGame = () => {
+    setPlayerChoice(null);
+    setComputerChoice(null);
+    setResult(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Rock-Paper-Scissors</h1>
+      {result ? (
+        <>
+          <p>You chose: {playerChoice}</p>
+          <p>Computer chose: {computerChoice}</p>
+          <p>{result}</p>
+          <button onClick={resetGame}>Play Again</button>
+        </>
+      ) : (
+        <>
+          <p>Choose your weapon:</p>
+          {choices.map((choice) => (
+            <button key={choice} onClick={() => handlePlayerChoice(choice)}>
+              {choice}
+            </button>
+          ))}
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
